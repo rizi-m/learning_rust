3d431410598dd4132002eb42b09ecec480dc50ea
Rizwan Mahmood <rizwan@pop-os.localdomain>
2020-06-23 15:36:36 +0100

    primitives

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(m: Matrix) -> Matrix {
+    Matrix(m.0, m.2, m.1, m.3)
+}
+
 fn main() {
-    println!("Hello, world!");
+    matrix_display();
+}
+
+fn matrix_display() {
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix))
 }
